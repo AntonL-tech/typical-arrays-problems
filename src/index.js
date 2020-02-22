52e8813e955ef807455e04e3e1518c7380b9d7ce
Anton <antonlashyk@gmail.com>
2020-02-22 15:24:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length <= 0) {
+    return 0;
+  }
+  array.sort((a,b) => a - b);
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length <= 0) {
+    return 0;
+  }
+  array.sort((a,b) => a - b);
+  return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length <= 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++ ) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
